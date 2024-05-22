340874613d37d58cdb5259440d0636f010cca08e
Al Vi <vaila.specter@gmail.com>
2024-05-22 22:46:53 +0300

    fix lines

--- lesson_2/lesson_2_task_10.py
+++ lesson_2/lesson_2_task_10.py
@@ -3,5 +3,4 @@ def bank(x,y):
     for i in range(y):
         x = x + (x+percent)
     return print(round(x))
-
 bank(1000, 10)
--- lesson_2/lesson_2_task_3.py
+++ lesson_2/lesson_2_task_3.py
@@ -2,20 +2,4 @@ import math
 a = int(input())
 def square(a):
     return math.ceil(a * a)
-
 print(square(a))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
