a93ec7d3d2e677b59a4fcfc230a7f5ddaab1ea7f
Al Vi <vaila.specter@gmail.com>
2024-06-03 22:18:48 +0300

    lil changes

--- lesson_5/Клик по кнопке с CSS-классом.py
+++ lesson_5/Клик по кнопке с CSS-классом.py
@@ -4,28 +4,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
-from time import sleep
-chrome = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-firefox = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 try:
- chrome.get("https://uitestingplayground.com/classattr")
- firefox.get("https://uitestingplayground.com/classattr")
- countC = 0
- countF = 0
+ browser.get("https://uitestingplayground.com/classattr")
+ count = 0
  for x in range(3):
-  chrome.find_element(By.XPATH, "//button[contains(@class, 'btn-primary')]").click()
-  countC = countC+1
-  chrome.switch_to.alert.accept()
-  firefox.find_element(By.XPATH, "//button[contains(@class, 'btn-primary')]").click()
-  countF = countF+1
-  firefox.switch_to.alert.accept()
+  browser.find_element(By.XPATH, "//button[contains(@class, 'btn-primary')]").click()
+  count = count + 1
+  browser.switch_to.alert.accept()
 
- print(f'Кнопка Chrome нажата {countC} раза')  
- print(f'Кнопка Firefox нажата {countF} раза') 
+ print(f'Кнопка нажата {count} раза') 
 
 except Exception as ex:
  print(ex)
 finally:
- chrome.quit()
- firefox.quit()
+ browser.quit()
