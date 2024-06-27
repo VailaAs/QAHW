dc741306720d53098c5b4aadaba1a975de622467
Al Vi <vaila.specter@gmail.com>
2024-06-28 01:54:50 +0300

    hw8

--- lesson_8/swagger_api.py
+++ lesson_8/swagger_api.py
@@ -8,21 +8,66 @@ def url():
     yield url
 
 
-class CompanyApi:
-
+class SwaggerApi:
     def __init__(self, url):
         self.url = url
-  
-# auth company
-    def company_auth(self):
-        user = "dude"
-        password = "cool"
-        response = requests.post(f"{self.url}auth/login", json={"username": user, "password": password})
+
+    def company_auth_get_token(self):
+        user = "bloom"
+        passw = "fire-fairy"
+        response = requests.post(f"{self.url}auth/login",
+                                json={"username": user, "password": passw})
+        token = response.json()["userToken"]
+        return token
+
+    def company_create_get_id(self, token):
+        my_headers = {'x-client-token': token}
+        name = 'test_comp'
+        description = 'try out for test'
+        response = requests.post(f"{self.url}company", headers=my_headers,
+                                json={'name': name, 'description': description})
+        company_id = response.json()['id']
+        return company_id
+
+    def company_delete(self, token, id):
+        my_headers = {'x-client-token': token}
+        response = requests.get(f"{self.url}/company/delete/{id}",
+                                headers=my_headers)
+        return response.status_code
+
+    def get_all_employees(self, company_id):
+        my_params = {"company": company_id}
+        response = requests.get(f"{self.url}/employee", params=my_params)
         return response
 
+    def employee_body(self, id, first_name, last_name, middle_name, company_id, email, avatar_url, phone, birthdate):
+        body = {
+            "id": id,
+            "firstName": first_name,
+            "lastName": last_name,
+            "middleName": middle_name,
+            "companyId": company_id,
+            "email": email,
+            "url": avatar_url,
+            "phone": phone,
+            "birthdate": f"{birthdate}T00:00:00.000Z",
+            "isActive": 'true'
+        }
+        return body
 
-# check with user and pass
-#check only user
-#check only pass
-#check empty body
+    def employee_create(self, token, body):
+        my_headers = {'x-client-token': token}
+        response = requests.post(f"{self.url}/employee",
+                                 headers=my_headers, json=body)
+        employee_id = response.json()['id']
+        return employee_id
 
+    def employee_get_by_id(self, employee_id):
+        response = requests.get(f"{self.url}/employee/{employee_id}")
+        return response
+
+    def employee_patch(self, employee_id, token, body):
+        my_headers = {'x-client-token': token}
+        response = requests.patch(f"{self.url}/employee/{employee_id}",
+                                  headers=my_headers, json=body)
+        return response
--- lesson_8/test_swagger.py
+++ lesson_8/test_swagger.py
@@ -1,6 +1,128 @@
 from swagger_api import *
+import pytest
 
-def test_swag(url):
-    com = CompanyApi(url)
-    res = com.company_auth()
-    assert res.status_code == 201
+# get_employee
+def test_positive_get_emp(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    res = emp.get_all_employees(company_id)
+    assert res.status_code == 200
+    del_com = emp.company_delete(token, company_id)
+    assert del_com == 200
+
+@pytest.mark.xfail()
+@pytest.mark.parametrize('company_id', 
+    [None, 
+     '', 
+     ' ', 
+     'abc'
+    ])
+def test_negative_get_emp(url, company_id):
+    # no company_id
+    emp = SwaggerApi(url)
+    res = emp.get_all_employees(company_id)
+    assert res.status_code == 200
+
+# add_employee
+def test_positive_add_employee(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    com_emp_before = emp.get_all_employees(company_id).json()
+    new_employee_body = emp.employee_body(1, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    assert employee_id != None
+    com_emp_after = emp.get_all_employees(company_id).json()
+    assert (len(com_emp_after) - len(com_emp_before)) == 1
+
+@pytest.mark.xfail()
+def test_negative_add_employee_no_body(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(2, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, None)
+    assert employee_id != None
+
+@pytest.mark.xfail()
+def test_negative_add_employee_no_token(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(2, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(None, new_employee_body)
+    assert employee_id != None
+
+@pytest.mark.xfail()
+def test_negative_add_employee_nothing(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(2, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(None, None)
+    assert employee_id != None
+
+# get_employee_by_id
+def test_positive_get_employee_by_id(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(3, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    get_emp = emp.employee_get_by_id(employee_id)
+    assert get_emp.status_code == 200
+
+@pytest.mark.xfail()
+def test_negative_get_employee_by_id(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(4, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    get_emp = emp.employee_get_by_id(None)
+    assert get_emp.status_code == 200
+
+# patch_employee
+@pytest.mark.xfail()
+def test_positive_patch_employee(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(4, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    patch_body = {"phone": "+123456789"}
+    patched_employee = emp.employee_patch(employee_id, token, patch_body)
+    assert patched_employee.status_code == 201  # получаем код 200 вместо 201, тело неполное
+
+@pytest.mark.xfail()
+def test_negative_patch_employee_no_emp_id(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(4, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    patch_body = {"phone": "+123456789"}
+    patched_employee = emp.employee_patch(None, token, patch_body)
+    assert patched_employee.status_code == 201
+
+@pytest.mark.xfail()
+def test_negative_patch_employee_token(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(4, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    patch_body = {"phone": "+123456789"}
+    patched_employee = emp.employee_patch(employee_id, None, patch_body)
+    assert patched_employee.status_code == 201
+
+@pytest.mark.xfail()
+def test_negative_patch_employee_no_body(url):
+    emp = SwaggerApi(url)
+    token = emp.company_auth_get_token()
+    company_id = emp.company_create_get_id(token)
+    new_employee_body = emp.employee_body(4, "Anna", "L", "", company_id, "email1@email.com", "", "yes", "2002-06-24")
+    employee_id = emp.employee_create(token, new_employee_body)
+    patched_employee = emp.employee_patch(employee_id, token, None)
+    assert patched_employee.status_code == 201
